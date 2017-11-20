e2ee956da4babf879c3c06ac2843d8a36ecf93a8
lunaplush <lunaplus@mail.ru>
2017-11-20 14:19:30 +0300

    neuro_work

--- mmro.py
+++ mmro.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn as skl
 from sklearn import svm
-from sklearn.cross_validation import train_test_split,cross_val_score
+from sklearn.cross_validation import train_test_split,cross_val_score,StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
@@ -21,6 +21,8 @@ X1,Y1,CLs1 = ps_data.open_ps_2007()
 
 features = ["ro","RON", "MON", "OLF","ALK","AROMA","BNZ","KIS", \
             "TLL","MASS","METANOL","ETANOL","MTBE","ETBE","TAME","DIPE","TBS"]
+features2 = ["ro","RON", "MON", "OLF","ALK","AROMA","BNZ","KIS", \
+            "TLL","MASS","ETANOL","MTBE","ETBE","TAME","DIPE","TBS"]
 
 
 #%%
@@ -38,26 +40,27 @@ X7["class_num"] = Y1["class_num"]
 #https://habrahabr.ru/post/304214/
 X7_92 = X7[(X7["class_num"] == 3) | (X7["class_num"] == 4) | (X7["class_num"] == 5)]
 
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler()
 
 
 #X = X7_92[X7.columns - ["class_num"]] - X7_92[X7.columns - ["class_num"]].mean()
-X7_92_x = X7_92[X7.columns - ["class_num","METANOL"]]
+X7_92_x = X7_92[list(set(X7.columns) - {"class_num","METANOL"})]
+#X7_92_x = X7_92[features2]
 minzn = X7_92_x.min(axis =0)
 maxzn = X7_92_x.max(axis =0)
 dif_min_max = maxzn-minzn
 
 
-X = scaler.fit_transform(X7_92[X7.columns - ["class_num"]])
+X = scaler.fit_transform( X7_92[list(set(X7.columns) - {"class_num","METANOL"})])
 y = X7_92["class_num"]-3
 
 #matrix_covariance = np.cov(X.T)
 #vectors = np.linalg.eig(matrix_covariance)
 #%%
 TRAIN_SIZE = 0.7 # Разделение данных на обучающую и контрольную части в пропорции 70/30%
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=TRAIN_SIZE, random_state=0)
 #%%
@@ -68,7 +71,7 @@ from pybrain.structure.modules import SoftmaxLayer
 from pybrain.utilities import percentError
 #%%
 # Определение основных констант
-HIDDEN_NEURONS_NUM = 100 # Количество нейронов, содержащееся в скрытом слое сети
+HIDDEN_NEURONS_NUM = 10 # Количество нейронов, содержащееся в скрытом слое сети
 MAX_EPOCHS = 100 # Максимальное число итераций алгоритма оптимизации параметров сети
 
 #%%
@@ -93,7 +96,7 @@ net = buildNetwork(ds_train.indim, HIDDEN_NEURONS_NUM, ds_train.outdim, outclass
 # ds.outdim -- количество нейронов выходного слоя, равное количеству меток классов
 # SoftmaxLayer -- функция активации, пригодная для решения задачи многоклассовой классификации
 
-init_params = np.random.random((len(net.params))) # Инициализируем веса сети для получения воспроизводимого результата
+init_params = np.random.random(( len(net.params))) # Инициализируем веса сети для получения воспроизводимого результата
 net._setParameters(init_params)
 #%%
 random.seed(0)
@@ -109,47 +112,63 @@ print('Error on train: ', percentError(res_train, ds_train['target'].argmax(axis
 res_test = net.activateOnDataset(ds_test).argmax(axis=1) # Подсчет результата на тестовой выборке
 print('Error on test: ', percentError(res_test, ds_test['target'].argmax(axis=1)), '%') # Подсчет ошибки
 
+                                      
 #%%
-random.seed(0) # Зафиксируем seed для получния воспроизводимого результата
+#Эксперимент для одного класса
+#[ 0 = 3, 1= 5, 2=5]
+class_num = []
+for i in X7_92.groupby("class_num"):
+    class_num.append(len(i[1]))
 
+class_num_current = 0    
+Xp = np.array((class_num[class_num_current],16))
+for i in np.arange(class_num[class_num_current]):
+    for j in np.arange(16):
+        Xp[i][j] = np.random.rand(1)
 
-def plot_classification_error(hidden_neurons_num, res_train_vec, res_test_vec):
-# hidden_neurons_num -- массив размера h, содержащий количество нейронов, по которому предполагается провести перебор,
-#   hidden_neurons_num = [50, 100, 200, 500, 700, 1000];
-# res_train_vec -- массив размера h, содержащий значения доли неправильных ответов классификации на обучении;
-# res_train_vec -- массив размера h, содержащий значения доли неправильных ответов классификации на контроле
-    plt.figure()
-    plt.plot(hidden_neurons_num, res_train_vec)
-    plt.plot(hidden_neurons_num, res_test_vec, '-r')
+#train_index, test_index  = StratifiedShuffleSplit(y,)
 
-def write_answer_nn(optimal_neurons_num):
-    with open("nnets_answer1.txt", "w") as fout:
-        fout.write(str(optimal_neurons_num))
-
-hidden_neurons_num = [50, 100, 200, 500, 700, 1000]
-res_train_vec = list()
-res_test_vec = list()
-#np.random.seed(0)
-#random.seed(0)
-for nnum in hidden_neurons_num:
-    
-    # Put your code here
-    # Не забудьте про инициализацию весов командой np.random.random((len(net.params)))
-   
-    net = buildNetwork(ds_train.indim, nnum, ds_train.outdim, outclass=SoftmaxLayer)
-    init_params = np.random.random((len(net.params))) # Инициализируем веса сети для получения воспроизводимого результата
-    net._setParameters(init_params)
-    
-  
-    trainer = BackpropTrainer(net, dataset=ds_train) # Инициализируем модуль оптимизации
-    err_train, err_val = trainer.trainUntilConvergence(maxEpochs=MAX_EPOCHS)
-    
-    res_train = net.activateOnDataset(ds_train).argmax(axis=1) 
-    res_train_vec.append(percentError(res_train, ds_train['target'].argmax(axis=1)))
-    res_test = net.activateOnDataset(ds_test).argmax(axis=1)
-    res_test_vec.append(percentError(res_test, ds_test['target'].argmax(axis=1)))
-    
-# Постройте график зависимости ошибок на обучении и контроле в зависимости от количества нейронов
-plot_classification_error(hidden_neurons_num, res_train_vec, res_test_vec)          
-#  Запишите в файл количество нейронов, при котором достигается минимум ошибки на контроле
-write_answer_nn(hidden_neurons_num[res_test_vec.index(min(res_test_vec))]) 
+#%%
+#random.seed(0) # Зафиксируем seed для получния воспроизводимого результата
+#
+#
+#def plot_classification_error(hidden_neurons_num, res_train_vec, res_test_vec):
+## hidden_neurons_num -- массив размера h, содержащий количество нейронов, по которому предполагается провести перебор,
+##   hidden_neurons_num = [50, 100, 200, 500, 700, 1000];
+## res_train_vec -- массив размера h, содержащий значения доли неправильных ответов классификации на обучении;
+## res_train_vec -- массив размера h, содержащий значения доли неправильных ответов классификации на контроле
+#    plt.figure()
+#    plt.plot(hidden_neurons_num, res_train_vec)
+#    plt.plot(hidden_neurons_num, res_test_vec, '-r')
+#
+#def write_answer_nn(optimal_neurons_num):
+#    with open("nnets_answer1.txt", "w") as fout:
+#        fout.write(str(optimal_neurons_num))
+#
+#hidden_neurons_num = [50, 100, 200, 500, 700, 1000]
+#res_train_vec = list()
+#res_test_vec = list()
+##np.random.seed(0)
+##random.seed(0)
+#for nnum in hidden_neurons_num:
+#    
+#    # Put your code here
+#    # Не забудьте про инициализацию весов командой np.random.random((len(net.params)))
+#   
+#    net = buildNetwork(ds_train.indim, nnum, ds_train.outdim, outclass=SoftmaxLayer)
+#    init_params = np.random.random((len(net.params))) # Инициализируем веса сети для получения воспроизводимого результата
+#    net._setParameters(init_params)
+#    
+#  
+#    trainer = BackpropTrainer(net, dataset=ds_train) # Инициализируем модуль оптимизации
+#    err_train, err_val = trainer.trainUntilConvergence(maxEpochs=MAX_EPOCHS)
+#    
+#    res_train = net.activateOnDataset(ds_train).argmax(axis=1) 
+#    res_train_vec.append(percentError(res_train, ds_train['target'].argmax(axis=1)))
+#    res_test = net.activateOnDataset(ds_test).argmax(axis=1)
+#    res_test_vec.append(percentError(res_test, ds_test['target'].argmax(axis=1)))
+#    
+## Постройте график зависимости ошибок на обучении и контроле в зависимости от количества нейронов
+#plot_classification_error(hidden_neurons_num, res_train_vec, res_test_vec)          
+##  Запишите в файл количество нейронов, при котором достигается минимум ошибки на контроле
+#write_answer_nn(hidden_neurons_num[res_test_vec.index(min(res_test_vec))]) 
