3f4f161454d2248a549c9ddd6efda93935fac8a2
lunaplush <lunaplus@mail.ru>
2018-01-23 10:53:55 +0300

    comment old net

--- neuro.py
+++ neuro.py
@@ -116,22 +116,23 @@ MAX_EPOCHS = 250
 
 
 #%%
-np.random.seed(0)
-net = FeedForwardNetwork()
-inLayer = LinearLayer(N)
-hiddenLayer = SigmoidLayer(HIDDEN_NEURONS_NUM)
-hiddenLayer2 = SigmoidLayer(HIDDEN_NEURONS_NUM2)
-outLayer = LinearLayer(1)
-
-net.addInputModule(inLayer)
-net.addModule(hiddenLayer)
-net.addModule(hiddenLayer2)
-net.addOutputModule(outLayer)
-
-net.addConnection(FullConnection(inLayer, hiddenLayer))
-net.addConnection(FullConnection(hiddenLayer,hiddenLayer2))
-net.addConnection(FullConnection(hiddenLayer2, outLayer))
-net.sortModules()
+#np.random.seed(0)
+#net = FeedForwardNetwork()
+#inLayer = LinearLayer(N)
+#hiddenLayer = SigmoidLayer(HIDDEN_NEURONS_NUM)
+#hiddenLayer2 = SigmoidLayer(HIDDEN_NEURONS_NUM2)
+#outLayer = LinearLayer(1)
+#
+#net.addInputModule(inLayer)
+#net.addModule(hiddenLayer)
+#net.addModule(hiddenLayer2)
+#net.addOutputModule(outLayer)
+#
+#net.addConnection(FullConnection(inLayer, hiddenLayer))
+#net.addConnection(FullConnection(hiddenLayer,hiddenLayer2))
+#net.addConnection(FullConnection(hiddenLayer2, outLayer))
+#net.sortModules()
+#%%
 #net = buildNetwork(ds_train.indim, HIDDEN_NEURONS_NUM, ds_train.outdim, bias=True,outclass=SoftmaxLayer)
 # ds.indim -- количество нейронов входного слоя, равне количеству признаков
 # ds.outdim -- количество нейронов выходного слоя, равное количеству меток классов
